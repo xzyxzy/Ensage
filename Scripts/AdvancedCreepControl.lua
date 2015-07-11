c506753abfc0797f76885768bf3a46a0636c7194
xzyxzy <xzyxzy@users.noreply.github.com>
2015-07-12 04:18:30 +0500

    Update AdvancedCreepControl.lua

--- Scripts/AdvancedCreepControl.lua
+++ Scripts/AdvancedCreepControl.lua
@@ -601,7 +601,7 @@ class 'Hero'
 						self.heroEntity:SafeCastAbility(impetus, target)
 					else entityList:GetMyPlayer():Attack(target) end
 				else
-					mp:Attack(Target)
+					mp:Attack(target)
 				end
 			else
 				if self.heroEntity.classId == CDOTA_Unit_Hero_Clinkz then
@@ -610,7 +610,7 @@ class 'Hero'
 						self.heroEntity:SafeCastAbility(searinga, target)
 					else entityList:GetMyPlayer():Attack(target) end
 				else
-					mp:Attack(Target)
+					mp:Attack(target)
 				end
 			end
 		end
